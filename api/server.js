a9c563cf77b54265b23bef6e5f55481751df2f56
yogeshsaini469 <yogeshsaini469@gmail.com>
2024-03-13 11:28:35 +0530

    sddsaa

--- api/server.js
+++ api/server.js
@@ -1,39 +1,47 @@
-// /api/server.js
+// Import necessary modules
+const { Server } = require("socket.io");
+const { createServer } = require("@vercel/node");
 
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+// Create the Socket.IO server
+const io = new Server();
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+io.on("connection", (socket) => {
+  console.log(socket.id);
 
-app.get('/', (req, res) => res.send('Hello, WebRTC!!!!!!!'));
+  socket.emit("connection-success", {
+    status: "connection-success",
+    socketId: socket.id,
+  });
 
-io.of('/webRTCPeers').on('connection', socket => {
-    console.log(socket.id);
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} has disconnected.`);
+  });
 
-    socket.emit('connection-success', {
-        status: 'connection-success',
-        socketId: socket.id,
-    });
+  socket.on("sdp", (data) => {
+    socket.broadcast.emit("sdp", data);
+  });
 
-    socket.on('disconnect', () => {
-        console.log(`${socket.id} has disconnected.`);
-    });
+  socket.on("candidate", (data) => {
+    socket.broadcast.emit("candidate", data);
+  });
 
-    socket.on('sdp', data => {
-        socket.broadcast.emit('sdp', data);
-    });
-
-    socket.on('candidate', data => {
-        socket.broadcast.emit('candidate', data);
-    });
-
-    socket.on('callEnded', () => {
-        console.log('Call ended');
-        io.of('/webRTCPeers').emit('callEnded');
-    });
+  socket.on("callEnded", () => {
+    console.log("Call ended");
+    io.emit("callEnded");
+  });
 });
 
-module.exports = app;
+// Export the serverless function
+module.exports = (req, res) => {
+  // Handling HTTP requests
+  if (req.url === "/webrtc" && req.method === "GET") {
+    // Handle GET request to /webrtc
+    res.status(200).send("Hello, WebRTC!!!!!!!");
+  } else {
+    // For other routes or methods, return 404
+    res.status(404).send("Not found");
+  }
+};
+
+// Initialize the Socket.IO server
+io.attach(createServer());
