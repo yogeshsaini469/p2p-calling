e2ea5e2d0044155b1e7035670f6a1f6132a5cd3d
yogeshsaini469 <yogeshsaini469@gmail.com>
2024-03-13 11:43:11 +0530

    fg

--- api/server.js
+++ api/server.js
@@ -1,47 +1,33 @@
-// Import necessary modules
 const { Server } = require("socket.io");
-const { createServer } = require("@vercel/node");
 
-// Create the Socket.IO server
-const io = new Server();
-
-io.on("connection", (socket) => {
-  console.log(socket.id);
-
-  socket.emit("connection-success", {
-    status: "connection-success",
-    socketId: socket.id,
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`${socket.id} has disconnected.`);
-  });
-
-  socket.on("sdp", (data) => {
-    socket.broadcast.emit("sdp", data);
-  });
-
-  socket.on("candidate", (data) => {
-    socket.broadcast.emit("candidate", data);
-  });
-
-  socket.on("callEnded", () => {
-    console.log("Call ended");
-    io.emit("callEnded");
-  });
-});
-
-// Export the serverless function
 module.exports = (req, res) => {
-  // Handling HTTP requests
-  if (req.url === "/webrtc" && req.method === "GET") {
-    // Handle GET request to /webrtc
-    res.status(200).send("Hello, WebRTC!!!!!!!");
-  } else {
-    // For other routes or methods, return 404
-    res.status(404).send("Not found");
-  }
-};
+    if (!res.socket.server.io) {
+        console.log("First use, starting socket.io");
 
-// Initialize the Socket.IO server
-io.attach(createServer());
+        const io = new Server(res.socket.server);
+
+        io.of('/webRTCPeers').on('connection', (socket) => {
+            console.log(socket.id);
+
+            socket.emit('connection-success', {
+                status: 'connection-success',
+                socketId: socket.id,
+            });
+
+            socket.on('disconnect', () => {
+                console.log(`${socket.id} has disconnected.`);
+            });
+
+            // Handle other socket events (sdp, candidate, callEnded) here...
+
+            socket.on('callEnded', () => {
+                console.log('Call ended');
+                io.of('/webRTCPeers').emit('callEnded');
+            });
+        });
+
+        res.socket.server.io = io;
+    }
+
+    res.end();
+};
