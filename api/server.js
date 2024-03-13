e5570fa45fabd7485bf9af72f33026d1238535e2
yogeshsaini469 <yogeshsaini469@gmail.com>
2024-03-13 11:06:18 +0530

    update

--- api/server.js
+++ api/server.js
@@ -1,44 +1,40 @@
-const { Server } = require("socket.io");
+// /api/webrtc.js
 
-// Handle socket.io connections
-const handleSocketConnections = (io) => {
-  io.on("connection", (socket) => {
+const express = require('express');
+const serverless = require('serverless-http');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get('/', (req, res) => res.send('Hello, WebRTC!!!!!!!'));
+
+io.of('/webRTCPeers').on('connection', socket => {
     console.log(socket.id);
 
-    socket.emit("connection-success", {
-      status: "connection-success",
-      socketId: socket.id,
+    socket.emit('connection-success', {
+        status: 'connection-success',
+        socketId: socket.id,
     });
 
-    socket.on("disconnect", () => {
-      console.log(`${socket.id} has disconnected.`);
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} has disconnected.`);
     });
 
-    socket.on("sdp", (data) => {
-      console.log(data);
-      socket.broadcast.emit("sdp", data);
+    socket.on('sdp', data => {
+        socket.broadcast.emit('sdp', data);
     });
 
-    socket.on("candidate", (data) => {
-      console.log(data);
-      socket.broadcast.emit("candidate", data);
+    socket.on('candidate', data => {
+        socket.broadcast.emit('candidate', data);
     });
 
     socket.on('callEnded', () => {
-      console.log('Call ended');
-      io.emit('callEnded');
+        console.log('Call ended');
+        io.of('/webRTCPeers').emit('callEnded');
     });
-  });
-};
+});
 
-// Export the serverless function handler
-module.exports = async (req, res) => {
-  // Initialize socket.io server
-  const io = new Server();
-
-  // Handle socket.io connections
-  handleSocketConnections(io);
-
-  // Send response for HTTP request
-  res.status(200).json({ message: 'Hello from Vercel serverless function!' });
-};
+module.exports.handler = serverless(app);
