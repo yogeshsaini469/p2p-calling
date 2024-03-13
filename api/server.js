66ca7879ce8eebda59f22e071ebdeb81363abdcb
yogeshsaini469 <yogeshsaini469@gmail.com>
2024-03-13 10:42:18 +0530

    server updated

--- api/server.js
+++ api/server.js
@@ -1,49 +1,5 @@
-const { Server } = require("socket.io");
+// api/server.js
 
-// Initialize socket.io server
-const io = new Server();
-
-// Handle socket.io connections
-io.on("connection", (socket) => {
-  console.log(socket.id);
-
-  socket.emit("connection-success", {
-    status: "connection-success",
-    socketId: socket.id,
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`${socket.id} has disconnected.`);
-  });
-
-  socket.on("sdp", (data) => {
-    console.log(data);
-    socket.broadcast.emit("sdp", data);
-  });
-
-  socket.on("candidate", (data) => {
-    console.log(data);
-    socket.broadcast.emit("candidate", data);
-  });
-
-  socket.on('callEnded', () => {
-    console.log('Call ended');
-    io.emit('callEnded');
-  });
-});
-
-// Export the serverless function handler
 module.exports = async (req, res) => {
-  // Set up socket.io connection
-  io.httpServer = {
-    on: (event, cb) => {
-      if (event === 'request') {
-        cb(req, res);
-      }
-    }
-  };
-  io.attach(io.httpServer);
-
-  // Handle socket.io events
-  await io.httpServer.emit('connection', { socket: true });
+  res.status(200).json({ message: 'Hello from Vercel serverless function!' });
 };
